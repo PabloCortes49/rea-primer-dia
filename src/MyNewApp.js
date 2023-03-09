a6c0e867b3016f69caa93ffe0f690ee97fdbc5ef
ggonzaloo10 <92878187+GitSepape@users.noreply.github.com>
2023-03-09 20:56:57 +0100

    subo cambios

--- src/MyNewApp.js
+++ src/MyNewApp.js
@@ -1,5 +1,41 @@
 
 import Contact from "./components/Contact/Contact";
+import {Header} from "./components/Header/Header";
 
-const MyNewApp
-const
+const myContacts = [
+    {
+        name: 'Josemi',
+        imageUrl: 'https://ca.slack-edge.com/T0467LBA3HP-U045V14CLBV-f0a2123981ce-512',
+        email: 'josemi@streamloots.com',
+    },
+    {
+        name: 'Josemi',
+        imageUrl: 'https://ca.slack-edge.com/T0467LBA3HP-U045V14CLBV-f0a2123981ce-512',
+        email: 'jjjj@streamloots.com',
+    },
+    {
+        name: 'Pepemi',
+        imageUrl: 'https://ca.slack-edge.com/T0467LBA3HP-U045V14CLBV-f0a2123981ce-512',
+        email: 'pepemi@streamloots.com',
+    }
+]
+
+const MyNewApp = () => {
+    return <div>
+        <Header>
+            <span>My contacts</span>
+        </Header>
+       {myContacts.map(element => <Contact name={element.name} imageUrl={element.imageUrl} email={element.email} key={element.email}/>)}
+    </div>
+}
+
+export default MyNewApp;
+
+
+
+
+
+
+
+
+
---
+++ src/contact.js
@@ -0,0 +1,7 @@
+export const Header = ({children}) => {
+    return (
+      <h1 className="header">
+        {children}
+      </h1>
+    );
+  };
